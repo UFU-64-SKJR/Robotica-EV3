e4803a209774851fece5df368eb65e1214928e53
Kaio Souza <38025621+Aureom@users.noreply.github.com>
2019-10-24 15:49:29 -0300

    Update exercicio5.c

--- 3/exercicio5.c
+++ 3/exercicio5.c
@@ -4,36 +4,37 @@
  maior ou igual a 10 e menor que 20 siga em frente. Se for maior ou igual que 20 e menor
  ou igual que 30, vire à direita. Cada função de movimentação deve acontecer por 1 segundo.
  Dica: utilize a função srand().
-*/
+ */
 
 #include <ev3.h>
 
 int main(void) {
 
+	InitEV3();
+
 	int random = 0;
 
-	srand(time(0));
+	while(ButtonIsDown(BTNDOWN) == 0){ // Botão do brick não pressionado
+		random = rand() % (30 + 1 - 0) + 0; // random entre 0 e 30
+		LcdPrintf(1, "%d, ", random);
 
-	while(1){
-		if(ButtonIsDown(BTNDOWN) == 0){ // Botão do brick não pressionado
-			random = rand() % (30 + 1 - 0) + 0; // random entre 0 e 30
-		}else{ // Botão do brick pressionado
-			if(random < 10){
-				OnFwdReg(OUT_A, -30);
-				OnFwdReg(OUT_C, 30);
-				Wait(SEC_1);
-			}
-			if(random >= 10 && random < 20){
-				OnFwdReg(OUT_AC, 30);
-				Wait(SEC_1);
-			}
-			if(random >= 20 && random <= 30){
-				OnFwdReg(OUT_A, 30);
-				OnFwdReg(OUT_C, -30);
-				Wait(SEC_1);
-			}
+		if(random < 10){
+//			OnFwdReg(OUT_A, -30);
+			OnFwdReg(OUT_C, 30);
+			Wait(SEC_1);
+			Off(OUT_C);
+		}else if(random >= 10 && random < 20){
+			OnFwdReg(OUT_AC, 30);
+			Wait(SEC_1);
+			Off(OUT_AC);
+		}else if(random >= 20 && random <= 30){
+			OnFwdReg(OUT_A, 30);
+//			OnFwdReg(OUT_C, -30);
+			Wait(SEC_1);
+			Off(OUT_A);
 		}
 	}
+
 
 	FreeEV3();
 	return 0;
