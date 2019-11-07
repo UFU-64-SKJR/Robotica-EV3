2a61b98e024f16d5e874877e3af9744a4041a9b9
Kaio Souza <38025621+Aureom@users.noreply.github.com>
2019-11-07 14:44:17 -0300

    Create exercicio3.c

---
+++ 4/exercicio3.c
@@ -0,0 +1,38 @@
+/*
+  Muitas vezes, quando programamos o robô, os motores não operam sincronizados, o que
+resulta no robô tendendo para o lado do motor que exerce a menor força. Neste desafio você
+irá resolver esse problema. Usando o sensor giroscópio, sem o auxílio de outros sensores, faça
+o robô andar em linha reta, corrigindo sua trajetória conforme ele desvie para as laterais
+ */
+
+#include <ev3.h>
+
+int main(void) {
+
+	InitEV3();
+
+	int motorSpeedA = 25, motorSpeedC = 25;
+	int gyroscope = setAllSensorMode(GYRO_ANG, NO_SEN, NO_SEN, NO_SEN);
+
+	if(gyroscope == 0) {
+		while(ButtonIsDown(BTNDOWN) == 0){ // Botão do brick não pressionado
+			int initAxis;
+			initAxis = readSensor(IN_1);
+
+			OnFwrReg(OUT_A, motorSpeedA);
+			OnFwrReg(OUT_C, motorSpeedC);
+			while(initAxis != readSensor(IN_1)) {
+				int actualAxis = readSensor(IN_1);
+				if(initAxis > actualAxis){
+					motorSpeedA++;
+				}else{
+					motorSpeedC++;
+				}
+			}
+		}
+	}
+
+
+	FreeEV3();
+	return 0;
+}
